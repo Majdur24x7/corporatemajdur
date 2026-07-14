b8341030396b90a0c374afcf6854d88ba6b288b1
Majdur24x7 <corporatemazdoor24x7@gmail.com>
2026-07-14 14:39:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Corporate Majdur Loaded 🚀");
+function incrementViewCount(row) {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var cell = sheet.getRange(row, 2); // Column B mein count hai
+  var currentValue = cell.getValue();
+  cell.setValue(currentValue + 1);
+}
