08f610b281292883cc82b777e94a3fadb2f46d33
Corporate Mazdur <corporatemazdoor24x7@gmail.com>
2026-08-01 18:11:08 +0530

    Create memes.js

---
+++ memes.js
@@ -0,0 +1,22 @@
+const allMemesData = {
+    "office-life": [
+        {
+            image: "images/meme-sample-1.jpg",
+            title: "When boss says \"We are a family here\" right before cutting the budget",
+            likes: "342"
+        },
+        {
+            image: "images/meme-sample-2.jpg",
+            title: "Me pretending to look busy when the manager walks past my desk",
+            likes: "528"
+        }
+    ],
+    "managers": [
+        {
+            image: "images/meme-sample-3.jpg",
+            title: "When manager asks for status update on a task you just started",
+            likes: "890"
+        }
+    ]
+    // Aise hi aap apni baki ki 8 categories yahan niche add kar sakte hain!
+};
